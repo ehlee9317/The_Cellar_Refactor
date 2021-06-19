a4f610e30071d4d3af7120164092f264f1706cfa
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-06-18 22:53:26 -0400

    edit page refactoring

--- client/components/editWine2.js
+++ client/components/editWine2.js
@@ -49,34 +49,53 @@ const useStyles = makeStyles(() => ({
 }))
 
 const EditWine = props => {
-  // const [wineId, setWineId] = useState(props.match.params.wineId);
-  // const [name, setName] = useState('');
+  // set States
+  const [wineId, setWineId] = useState(props.match.params.wineId)
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [type, setType] = useState('')
+  const [year, setYear] = useState('')
+  const [origin, setOrigin] = useState('')
+  const [description, setDescription] = useState('')
+  const [imageUrl, setImageUrl] = useState('')
 
-  const [state, setState] = useState({
-    wineId: props.match.params.wineId,
-    name: '',
-    price: '',
-    type: '',
-    year: '',
-    origin: '',
-    description: '',
-    imageUrl: ''
-  })
+  // const [state, setState] = useState({
+  //   wineId: props.match.params.wineId,
+  //   name: '',
+  //   price: '',
+  //   type: '',
+  //   year: '',
+  //   origin: '',
+  //   description: '',
+  //   imageUrl: ''
+  // })
 
   //set Errors
-  const [errorMessage, setErrorMessage] = useState({
-    errorName: false,
-    errorPrice: false,
-    // errorType: false,
-    errorYear: false,
-    errorOrigin: false,
-    errorDescription: false
-  })
+  const [errorName, setErrorName] = useState(false)
+  const [errorPrice, setErrorPrice] = useState(false)
+  const [errorYear, setErrorYear] = useState(false)
+  const [errorOrigin, setErrorOrigin] = useState(false)
+  const [errorDescription, setErrorDescription] = useState(false)
 
+  // const [errorMessage, setErrorMessage] = useState({
+  //   errorName: false,
+  //   errorPrice: false,
+  //   errorYear: false,
+  //   errorOrigin: false,
+  //   errorDescription: false
+  // })
+
+  // set Helper Text
   const [nameHelperText, setNameHelperText] = useState('Name field is required')
-
   const [priceHelperText, setPriceHelperText] = useState(
     'Price field is required'
+  )
+  const [yearHelperText, setYearHelperText] = useState('Year field is required')
+  const [originHelperText, setOriginHelperText] = useState(
+    'Origin field is required'
+  )
+  const [descriptionHelperText, setDescriptionHelperText] = useState(
+    'Description field is required'
   )
 
   //Prep the tools
@@ -89,7 +108,7 @@ const EditWine = props => {
   useEffect(
     () => {
       async function getWine() {
-        if (state.wineId) await dispatch(fetchSingleWines(state.wineId))
+        if (wineId) await dispatch(fetchSingleWines(wineId))
       }
       getWine()
     },
@@ -100,16 +119,23 @@ const EditWine = props => {
     () => {
       function setWine(wines) {
         if (wines) {
-          setState({
-            ...state,
-            name: wines.name,
-            price: wines.price,
-            type: wines.type,
-            year: wines.year,
-            origin: wines.origin,
-            description: wines.description,
-            imageUrl: wines.imageUrl
-          })
+          // setState({
+          //   ...state,
+          //   name: wines.name,
+          //   price: wines.price,
+          //   type: wines.type,
+          //   year: wines.year,
+          //   origin: wines.origin,
+          //   description: wines.description,
+          //   imageUrl: wines.imageUrl
+          // })
+          setName({name: wines.name})
+          setPrice({price: wines.price})
+          setType({type: wines.type})
+          setYear({year: wines.year})
+          setOrigin({origin: wines.origin})
+          setDescription({description: wines.description})
+          setImageUrl({imageUrl: wines.imageUrl})
         }
       }
       setWine(wineInfo)
@@ -119,87 +145,163 @@ const EditWine = props => {
 
   //handle change function - Updating textfield
   const handleChange = event => {
-    setState({...state, [event.target.id]: event.target.value})
+    // setState({...state, [event.target.id]: event.target.value})
+    setName({name: event.target.value})
+    setPrice({price: event.target.value})
+    setYear({year: event.target.value})
+    setOrigin({origin: event.target.value})
+    setDescription({description: event.target.value})
+    setImageUrl({imageUrl: event.target.value})
   }
 
   //handle change function - Updating drop down
   const handleDropdownChange = event => {
-    setState({...state, type: event.target.value})
+    setType({type: event.target.value})
   }
 
+  // eslint-disable-next-line complexity
   const handleSubmit = () => {
-    const {name, price, year, origin, description} = state
+    // const {name, price, year, origin, description} = state
 
-    const {
-      errorName,
-      errorPrice,
-      errorYear,
-      errorOrigin,
-      errorDescription
-    } = errorMessage
+    // const {
+    //   errorName,
+    //   errorPrice,
+    //   errorYear,
+    //   errorOrigin,
+    //   errorDescription
+    // } = errorMessage
 
     // Check to make sure below input fields are NOT EMPTY
-    setErrorMessage({
-      errorName: name === '',
-      errorPrice: price === '',
-      errorYear: year === '',
-      errorOrigin: origin === '',
-      errorDescription: description === ''
-    })
+    // setErrorMessage({
+    //   errorName: name === '',
+    //   errorPrice: price === '',
+    //   errorYear: year === '',
+    //   errorOrigin: origin === '',
+    //   errorDescription: description === ''
+    // })
+    setErrorName({errorName: name === ''})
+    setErrorPrice({errorName: price === ''})
+    setErrorYear({errorName: year === ''})
+    setErrorOrigin({errorName: origin === ''})
+    setErrorDescription({errorName: description === ''})
 
     // // Check to make sure Name doesn't have any special characters
     // if(name !== name.replace(/[^A-Za-z0-9 ]/g, "")) {
     //   setErrorMessage({ errorName: true });
     //   setNameHelperText('Please remove any special characters.');
+    // } else if(name === '') {
+    //   setErrorMessage({ errorName: true });
+    //   setNameHelperText('Name field is required');
     // } else {
     //   setErrorMessage({ errorName: false });
-    //   setNameHelperText("Name field is required");
+    //   setNameHelperText(" ");
+    // }
+
+    // if(name !== name.replace(/[^A-Za-z0-9 ]/g, "") || name === "") {
+    //   setErrorMessage({
+    //     ...errorMessage,
+    //     errorName: true
+    //   });
+    //   setNameHelperText('Name field is required (no special characters)');
+    // }
+    // else {
+    //   setErrorMessage({
+    //     ...errorMessage,
+    //     errorName: false
+    //   });
+    //   setNameHelperText(" ");
     // }
 
     // Check to make sure price is NUMBER
-    if (isNaN(price)) {
-      setErrorMessage({errorPrice: true})
-      setPriceHelperText('Please only input numbers for price.')
-    } else if (price === '') {
-      setErrorMessage({errorPrice: true})
-      setPriceHelperText('Price field is required')
-    } else {
-      setErrorMessage({errorPrice: false})
-      setPriceHelperText(' ')
-    }
-
-    // if( errorName === false && errorPrice === false && errorYear === false && errorOrigin === false && errorDescription === false) {
-    //   dispatch(updateWine(state));
+    // if (isNaN(price)) {
+    //   console.log("1111");
+    //   setErrorMessage({
+    //     ...errorMessage,
+    //     errorPrice: true
+    //   })
+    //   setPriceHelperText('Please only input numbers for price.')
+    // } else if (price === '') {
+    //   console.log("22222");
+    //   setErrorMessage({
+    //     ...errorMessage,
+    //     errorPrice: true
+    //   })
+    //   setPriceHelperText('Price field is required')
     // }
 
-    if (name && price && year && origin && description) {
-      dispatch(updateWine(state))
+    // if(isNaN(price) || price === '' ) {
+    //   setErrorMessage({
+    //     ...errorMessage,
+    //     errorPrice: true
+    //   })
+    //   setPriceHelperText("Price field is required (numbers only)")
+    // }
+
+    // if(isNaN(price) !== false && price !== '') {
+    //   console.log("~~~~ got here");
+    //   setErrorMessage({
+    //     ...errorMessage,
+    //     errorPrice: false
+    //   })
+    //   setPriceHelperText('')
+    // }
+
+    // Check to make sure year is Number and not past current year
+    // if(isNaN(year) || year > new Date().getFullYear() || year === '') {
+    //   console.log("year1111")
+    //   setErrorMessage({
+    //     ...errorMessage,
+    //     errorYear: true
+    //   });
+    //   // setYearHelperText(`Please only input numbers less than or equal to ${new Date().getFullYear()}`);
+    //   setYearHelperText('Please only input numbers less than or equal to 2020');
+    // }
+
+    // if(isNaN(year) !== false && year <= new Date().getFullYear() && year !== ''){
+    //   console.log("year2222")
+    //   setErrorMessage({
+    //     ...errorMessage,
+    //     errorYear: false
+    //   });
+    //   setYearHelperText('');
+    // }
+
+    if (
+      errorName === false &&
+      errorPrice === false &&
+      errorYear === false &&
+      errorOrigin === false &&
+      errorDescription === false
+    ) {
+      dispatch(updateWine(name, price, year, origin, description))
     }
 
-    console.log('@@@@ errorMessage', errorMessage)
+    console.log('@@@@ errorName => ', errorName)
+    console.log('@@@@ errorPrice => ', errorPrice)
+    console.log('@@@@ errorYear => ', errorYear)
+    console.log('@@@@ errorOrigin => ', errorOrigin)
+    console.log('@@@@ errorDescription => ', errorDescription)
   }
 
-  const {
-    errorName,
-    errorPrice,
-    errorYear,
-    errorOrigin,
-    errorDescription
-  } = errorMessage
+  // const {
+  //   errorName,
+  //   errorPrice,
+  //   errorYear,
+  //   errorOrigin,
+  //   errorDescription
+  // } = errorMessage
 
   //Form template
   const form = () => {
     return (
       <form className={classes.form} onSubmit={handleSubmit} name={name}>
-        ;
-        <h1 className={classes.title}>Edit {wineInfo.name}</h1>;
+        <h1 className={classes.title}>Edit {wineInfo.name}</h1>;;;
         <FormTextField
           error={errorName}
           labelName="Wine Name"
           id="name"
           value={state.name}
           onChange={handleChange}
-          // helperText="Wine Name field is required"
           helperText={nameHelperText}
         />
         <FormTextField
@@ -208,7 +310,6 @@ const EditWine = props => {
           id="price"
           value={state.price}
           onChange={handleChange}
-          // helperText="Price field is required"
           helperText={priceHelperText}
         />
         <FormControl required>
@@ -237,7 +338,7 @@ const EditWine = props => {
           id="year"
           value={state.year}
           onChange={handleChange}
-          helperText="Year field is required"
+          helperText={yearHelperText}
         />
         <FormTextField
           error={errorOrigin}
@@ -245,7 +346,7 @@ const EditWine = props => {
           id="origin"
           value={state.origin}
           onChange={handleChange}
-          helperText="Origin field is required"
+          helperText={originHelperText}
         />
         <FormTextField
           error={errorDescription}
@@ -253,7 +354,7 @@ const EditWine = props => {
           id="description"
           value={state.description}
           onChange={handleChange}
-          helperText="Description field is required"
+          helperText={descriptionHelperText}
         />
         <FormTextField
           // error={state.errorName}
